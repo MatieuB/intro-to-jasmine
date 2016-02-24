e8114027c7e00ec995e050e772442d892cdc96ed
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-02-24 11:53:54 -0700

    can find if enough gas for given route

--- spec/miles_spec.js
+++ spec/miles_spec.js
@@ -1,7 +1,19 @@
-var miles = require ('../lib/miles')
+var miles = require ('../lib/miles');
 
 describe('Miles', function () {
-  it('our plan here', function () {  //anonymous callback function
-    expect(miles.someFunction()).toEqual("running stuff for real")
+  it('determines if cal has enough gas', function () {
+      var car = {
+        milesPerGallon: 20,
+        gallonsInTank: 3
+      };
+
+      var route = {
+        miles: 200,
+        destination: "Boulder",
+        location: 'Fancy Land'
+      };
+    expect(miles.hasEnoughGas(car, route)).toEqual(false);
+    car.gallonsInTank += 20;
+    expect(miles.hasEnoughGas(car, route)).toEqual(true);
   });
-})
+});
